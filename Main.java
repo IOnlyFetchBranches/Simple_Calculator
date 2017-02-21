6e088f51598b2f4e5498c8e893c8cd89c5bc6e1b
demarcus-joachim <llmuzical@gmail.com>
2017-02-21 13:20:06 -0500

    Test

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         Pane root =  FXMLLoader.load(Main.class.getResource("frame.fxml"));
 
 
