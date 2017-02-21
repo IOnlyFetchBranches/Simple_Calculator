16b3dc9c45850ac98c81c79613816b36b921f295
demarcus-joachim <llmuzical@gmail.com>
2017-02-21 13:10:14 -0500

    First

---
+++ Main.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root =  FXMLLoader.load(Main.class.getResource("frame.fxml"));
+
+
+
+
+
+
+
+        Scene scene1=new Scene(root);
+        primaryStage.setScene(scene1);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
