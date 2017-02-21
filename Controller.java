52a9fde252699fdb6f7983b00a87eac6bdef80ee
demarcus-joachim <llmuzical@gmail.com>
2017-02-21 13:39:36 -0500

    Controller

---
+++ Controller.java
@@ -0,0 +1,38 @@
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
+    @FXML
+    private Button enter;
+    @FXML
+    private Button leftPar;
+    @FXML
+    private Button rightPar;
+    @FXML
+    private Button clear;
+    @FXML
+    private Button addButton,subButton,divideButton,multButton;
+
+
+
+
+
+}
